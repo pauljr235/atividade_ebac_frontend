896d39097b836a2fd72f72d64d532854b22915ef
Paulo Junior <148215124+pauljr235@users.noreply.github.com>
2024-09-27 21:17:20 +0100

    Add files via upload

---
+++ ex.js
@@ -0,0 +1,13 @@
+"use strict";
+// Multiplicaão de 2 números
+function multiplicador(n1, n2) {
+    return n1 * n2;
+}
+const resultado = multiplicador(2, 3);
+console.log(resultado);
+//DIZENDO OLÁ
+function dizOi(nome) {
+    return `Olá, ${nome}`;
+}
+const saluto = dizOi("Paulo");
+console.log(saluto);
---
+++ ex.ts
@@ -0,0 +1,16 @@
+// Multiplicaão de 2 números
+
+function multiplicador (n1: number, n2: number): number {
+    return n1 * n2;
+}
+const resultado = multiplicador(2, 3);
+console.log(resultado);
+
+//DIZENDO OLÁ
+
+function dizOi(nome: string): string {
+    return `Olá, ${nome}`;
+    }
+    
+    const saluto = dizOi("Paulo" );
+    console.log(saluto);
